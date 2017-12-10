b298f3e637cbf8945a103bcedb8b183fea12ac03
a-staab <astaab@gmail.com>
2017-12-10 14:43:54 -0800

    User can add and remove subreddits to/from list.

--- script.js
+++ script.js
@@ -5,9 +5,44 @@ function add_new(subreddit) {
     subreddits.push(subreddit);
 }
 
+function removeSubreddit(subredditIndex) {
+
+    subreddits.splice(subredditIndex, 1);
+
+}
+
+function render() {
+
+    refreshSubreddits();
+    console.log(subreddits);
+
+}
+
+function refreshSubreddits() {
+
+    $('#subreddits').empty();
+    for (var i=0; i < subreddits.length; i++) {
+        var subredditName = "<li>" + subreddits[i] + "<button type='button' class='delete' id='delete-" + i + "'>" + "Delete</button></li>";
+        $('#subreddits').append(subredditName);
+    }
+    addDeleteListener();
+}
+
+
 $('#add-subreddit').on('click', function (evt) {
     evt.preventDefault();
     var subreddit = $('#subreddit').val();
     add_new(subreddit);
+    render();
+
+});
+
+function addDeleteListener(){
+    $('.delete').click(function (evt) {
+    evt.preventDefault();
+    var toRemove = parseInt(this.id.split("-")[1], 10);
+    removeSubreddit(toRemove);
+    render();
+});
 
 }
