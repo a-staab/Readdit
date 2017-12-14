b1cfdc6aa3f4f1e1c3b6993263043d3183052482
a-staab <astaab@gmail.com>
2017-12-13 16:06:17 -0800

    Added 'add' button and subreddits ul

--- script.js
+++ script.js
@@ -18,6 +18,36 @@ function render() {
 
 }
 
+function getPostsFor(subreddit) {
+
+    $.get("http://www.reddit.com/r/" + subreddit + "/new.json?sort=new&limit=5")
+    .then(function(response) {
+        var posts = response.data.children;
+        // Make a empty list 
+        for (i in posts) {
+            var data = posts[i].data
+            var author = data.author;
+            var text = data.selftext;
+            var created_at = data.created_utc;
+            var url = data.url;
+            var image
+            // make an image souce array
+            // make an object (JSON)
+            // add object to list
+        }
+
+        //resolve(object)
+
+
+
+    }, function(reason) {
+        alert("Unable to retrieve " + subreddit + " from Reddit.");
+        console.log(reason);
+    })
+
+}
+
+
 function refreshSubreddits() {
 
     $('#subreddits').empty();
@@ -25,7 +55,7 @@ function refreshSubreddits() {
         var subredditName = "<li>" + subreddits[i] + "<button type='button' class='delete' id='delete-" + i + "'>" + "Delete</button></li>";
         $('#subreddits').append(subredditName);
     }
-    addDeleteListener();
+    setDeleteListener();
 }
 
 
@@ -38,7 +68,7 @@ $('#add-subreddit').on('click', function (evt) {
 
 });
 
-function addDeleteListener(){
+function setDeleteListener(){
     $('.delete').click(function (evt) {
     evt.preventDefault();
     var toRemove = parseInt(this.id.split("-")[1], 10);
